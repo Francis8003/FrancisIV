32f4e93c0a6653664b8f1354668fbf6cc9d9a7d8
Francis8003 <18911338003.com>
2022-03-23 23:25:46 +0800

    新增一个基础事件参数，继承于框架事件基类。BaseEventArgs:GameFrameworkEventArgs: EventArgs,IReference 类成员只有一个Id

---
+++ FrameworkByFrancis/FrameworkByFrancis/Base/EventPool/BaseEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrameworkByFrancis
+{
+    /// <summary>
+    /// 事件基类
+    /// </summary>
+    public abstract class BaseEventArgs : GameFrameworkEventArgs
+    {
+        /// <summary>
+        /// 获取类型编号
+        /// </summary>
+        public abstract int Id
+        {
+            get;
+        }
+    }
+}
