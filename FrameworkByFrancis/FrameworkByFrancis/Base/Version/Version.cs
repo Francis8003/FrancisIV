c2bbb21fc43c82c95ff8a50b2238ac3db56a37e8
Francis8003 <18911338003.com>
2022-03-23 23:53:04 +0800

    新增了版本类，Version.cs  用了Partial分类写在两个脚本中，类名使用了Version.IVersionHelper.cs 的写法，用于表明是一个分支。只有表述版本号的获取，没有定义如何赋值（有可能是版本号不能被修改，所以不开放赋值入口，后续关注这个问题）。

---
+++ FrameworkByFrancis/FrameworkByFrancis/Base/Version/Version.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrameworkByFrancis
+{
+    /// <summary>
+    /// 版本号类
+    /// </summary>
+    public static partial class Version
+    {
+        private const string GameFrameworkVersionString = "2022.03.23";
+
+        private static IVersionHelper s_VersionHelper = null;
+
+        public static string GameframeworkVerison
+        {
+            get
+            {
+                return GameFrameworkVersionString;
+            }
+        }
+
+        ///<summary>
+        ///获取游戏版本号
+        /// </summary>
+        public static string GameVersion
+        {
+            get
+            {
+                if (s_VersionHelper == null)
+                {
+                    return string.Empty;
+                }
+                return s_VersionHelper.GameVersion;
+            }
+        }
+        /// <summary>
+        /// 获取内部游戏版本号
+        /// </summary>
+        public static int InternalGameVersion
+        {
+            get
+            {
+                if (s_VersionHelper == null)
+                {
+                    return 0;
+                }
+                return s_VersionHelper.InternalGameVersion;
+            }
+        }
+        public static void SetVersionHelper(IVersionHelper versionHelper)
+        {
+            s_VersionHelper = versionHelper;
+        }
+    }
+}
